f8b699fb8dd613883a175c6242831a9047c6d76e
Alissa <alissatroianonyc@gmail.com>
2022-07-15 21:44:07 -0400

    Update layout for travel tip cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
-// Temporary card flip animation - final solution will need to loop through all cards
-const card = document.querySelector('.card');
-card.addEventListener( 'click', function() {
-  card.classList.toggle('is-flipped');
-});
+// A function to make each card in guide.html flip when clicked
+const cards = document.querySelectorAll('.card');
+cards.forEach(card => {
+    card.addEventListener('click', function() {
+        card.classList.toggle('is-flipped');
+    });
+    }
+);
